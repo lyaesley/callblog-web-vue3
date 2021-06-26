c643eed0428f69059bdd4690521a8736cd03c0ab
lyae <lyaesley@gmail.com>
2021-06-27 00:38:46 +0900

    login api 호출 테스트 성공 CROS proxy vue.config.js로 해결

--- src/api/http.js
+++ src/api/http.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: process.env.VUE_APP_API
+    // baseURL: process.env.VUE_APP_API
+    // 여기에 설정하면 CROS 오류로 vue.config.js 에 proxy 설정으로 해결함
 });
 
 export default instance;
